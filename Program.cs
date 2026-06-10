9456dc9a03aaead84395e633bcdbed01c27c5b74
Beza <bezahailemariam2712@gmail.com>
2026-06-10 18:56:35 +0300

    Session 2 completed: DI, Options validation, Structured logging, Middleware

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+//ADD Service
+builder.Services
+    .AddAuthentication("Training")
+    .AddScheme<AuthenticationSchemeOptions,
+        TrainingAuthHandler>("Training", null);
+
+builder.Services.AddAuthorization();
+builder.Services.AddSingleton<EnrollmentWorker>();
+
+builder.Services.AddScoped<IEnrollmentService,
+    EnrollmentService>();
+    builder.Host.UseDefaultServiceProvider(options =>
+{
+    options.ValidateScopes = true;
+    options.ValidateOnBuild = true;
+});
+builder.Services.AddOptions<PaymentOptions>()
+    .BindConfiguration("Payments")
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+var app = builder.Build();
+
+//USE the Above Service
+app.UseMiddleware<RequestLoggingMiddleware>();
+
+app.UseExceptionHandler("/error");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapGet("/api/assessments/results", () =>
+{
+    return Results.Ok(new
+    {
+        courseCode = "CS-101",
+        studentId = "S-001",
+        letterGrade = "A"
+    });
+})
+.RequireAuthorization();
+
+app.Run();
